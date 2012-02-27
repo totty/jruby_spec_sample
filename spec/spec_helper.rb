c44a94eb49c459476dd7d97127c052fb773b75cf
Takahisa Tokunaga <tokunaga.jp@gmail.com>
2012-02-27 14:34:56 +0900

    "javac -d target/classes" 実行後に "rake spec" が実行できるようになりました

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+require 'java'
+
+$CLASSPATH << File.expand_path('../../target/classes/', __FILE__)
+
+Dir["#{File.dirname(__FILE__)}/../../lib/*.jar"].each do |jar|
+   require jar
+end
+
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :rspec
+end
